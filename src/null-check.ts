f89185c4b2ccfb86972070c5034e4b58745b0770
NIRAL\Neel <neel.dave@jarvistechnolabs.com>
2025-06-13 15:16:21 +0530

    first commit

---
+++ src/null-check.ts
@@ -0,0 +1,19 @@
+export const isNullOrUndefined = (s: any): boolean => {
+    return s === null || s === undefined;
+};
+
+export const isNullOrEmptyArray = (s: any[]): boolean => {
+    return isNullOrUndefined(s) || s.length === 0;
+};
+
+export const isNumberNullOrUndefined = (o: number): boolean => {
+    return isNullOrUndefined(o) || o.toString() === '';
+};
+
+export const isStringNullEmptyOrUndefined = (str: string): boolean => {
+    return isNullOrUndefined(str) || str === '';
+};
+
+export const isObjectEmpty = (obj: Object): boolean => {
+    return Object.keys(obj).length === 0;
+};
